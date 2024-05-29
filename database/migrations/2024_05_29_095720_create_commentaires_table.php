27e40d5c638814db79a669e6584221a0efdf31c3
mareme <mariamathiaw92@gmail.com>
2024-05-29 10:20:43 +0000

    Création du model Commentaire et de la colonne commentaires dans la base de donnée parti2_crud_article

---
+++ database/migrations/2024_05_29_095720_create_commentaires_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('commentaires', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_article')->constrained('articles')->onDelete('cascade');
+            $table->text('nom_complet_auteur');
+            $table->text('contenu');
+            $table->dateTime('date_heure_creation');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('commentaires');
+    }
+};
