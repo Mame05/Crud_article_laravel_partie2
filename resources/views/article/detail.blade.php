b15295cedd1c43f20b3611aa353da262f746cb04
mareme <mariamathiaw92@gmail.com>
2024-06-01 09:43:57 +0000

    suppresson des vue ajoutercom et listecom car j'ai tout intégré dans la vue detail, création de la route /ajouter/commentaire-traitement pour le formulaire d'ajout des commentaires de la fonction AjouterCommentaireTraitement et intégration du formulaire pour ajouter un commentaire dans la vue detail

--- resources/views/article/detail.blade.php
+++ resources/views/article/detail.blade.php
@@ -7,25 +7,42 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Détails de l'article N°{{$article->id}}</h1>
-    <div class="row">
-        <div class="col-sm-3">
-            <div class="card" style="width: 20rem;height:1000px">
-            <img src="{{ $article->image }}" class="card-img-top" alt="..." width="20rem" height="200px">
-            <div class="card-body">
-              <h5 class="card-title"><strong>Nom: {{ $article->nom }}</h5>
-              <p> <strong>Description : {{ $article->description}}</strong> </p>
-              <p><strong> Date de la création : {{$article->date_creation}}</strong></p>
-              <p class="d-inline-flex gap-3">
+    <div class="container">
+      <div class="row">
+        <div class="col s12">
+          <h1 class="text-center">Détails de l'article N°{{$article->id}}</h1>
+            <div class="card" style="width: 70rem">
+              <img src="{{ $article->image }}" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title"><strong>Nom: {{ $article->nom }}</h5>
+                <p> <strong>Description : {{ $article->description}}</strong> </p>
+                <p><strong> Date de la création : {{$article->date_creation}}</strong></p>
+                <p class="d-inline-flex gap-3">
                 <a href="/modifier-article/{{ $article->id }}" class="btn btn-outline-primary btn-sm">Modifier</a>
                 <a href="/supprimer-article/{{ $article->id }}" class="btn btn-outline-danger btn-sm">Supprimer</a>
-              </p>
-              <a href="/article" class="btn btn-outline-success btn-sm">Revenir à la liste des articles</a>
+                </p>
+                <a href="/article" class="btn btn-outline-success btn-sm">Revenir à la liste des articles</a>
+              </div>
+              <div >
+                <h1>LISTE DES COMMENTAIRES</h1>
+                <form action="/ajouter/commentaire-traitement" method="POST" class="form-group">
+                  @csrf
+                  <div class="form-group">
+                    <label for="nom_complet_auteur">Présentez-vous</label>
+                    <input type="text" class="form-control" id="nom_complet_auteur" name="nom_complet_auteur">
+                  </div>
+                  <div class="form-group">
+                    <label for="contenu">Votre commentaire</label>
+                    <textarea class="form-control" id="contenu" name="contenu"></textarea>
+                  </div>
+                <br>
+                  <button type="submit" class="btn btn-primary btn sm">Envoyer</button>
+                </form>
+              </div>
             </div>
-            </div>
-            <h1>LISTE DES COMMENTAIRES</h1>
         </div>
-
+      </div>
+    </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
+</body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -21,15 +21,20 @@ Route::get('/', function () {
                                  /* CRUD ARTICLE*/
 Route::get('/article', [ArticleController::class, 'ListeArticle']);
 
-Route::get('/ajouter', [ArticleController::class, 'AjouterArticle']);
-Route::post('/ajouter/traitement', [ArticleController::class, 'AjouterArticleTraitement']);
+Route::get('article/ajouter', [ArticleController::class, 'AjouterArticle']);
+Route::post('/ajouter/article-traitement', [ArticleController::class, 'AjouterArticleTraitement']);
 
 Route::get('/modifier-article/{id}', [ArticleController::class, 'ModifierArticle']);
-Route::post('/modifier/traitement', [ArticleController::class, 'ModifierArticleTraitement']);
+Route::post('/modifier/article-traitement', [ArticleController::class, 'ModifierArticleTraitement']);
 
 Route::get('/supprimer-article/{id}', [ArticleController::class, 'supprimer_article']);
 
 Route::get('/detail-article/{id}', [ArticleController::class, 'DetailArticle']);
                    
                                  /*CRUD COMMENTAIRE*/
-Route::get('/commentaire', [CommentaireController::class, 'ListeCommentaire']);
+/*Route::get('/commentaire', [CommentaireController::class, 'ListeCommentaire']);*/
+
+/*Route::get('commentaire/ajouter', [CommentaireController::class, 'AjouterCommentaire']);*/
+Route::post('/ajouter/commentaire-traitement', [CommentaireController::class, 'AjouterCommentaireTraitement']);
+
+/*On met le formulaire d'ajout de commentaire dans la vue détail c'est la raisou pour laquelle on a pas définie une route pour ajouter un commentaire mais si c'était le cas contraire on allait crée une route qui nous ménera vers cet formulaire*/
